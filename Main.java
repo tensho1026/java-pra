90721c469ca274d5042d4827c86680645c9bff7a
tensho1026 <tensho1026@icloud.com>
2025-10-01 23:04:16 +0900

    三項演算子まで

--- Main.java
+++ Main.java
@@ -1,14 +1,25 @@
 
 public class Main {
   public static void main(String[] args) {
-Main m = new Main();
-m.main2();
 
-Test.test();
-    
+
+    // double age = new java.util.Random().nextDouble(11.8);
+
+    // System.out.println(age);
+
+   int x = 5;
+   int y = 10;
+
+    System.out.println("x + yは" + (x + y));
+
+    // Main m = new Main();
+    // m.main2();
+
+    // Test.test();4ff
+
   }
 
-void main2 () {
-    System.out.println(600+593);
-}
+  // void main2 () {
+  // System.out.println(600+593);
+  // }
 }
--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 public class Test {
    public static void test() {
     System.out.println("Test Javaaaaaaaaaaaa");
+
+   boolean isTrue;
+   isTrue = false;
   }
+
 
 }
